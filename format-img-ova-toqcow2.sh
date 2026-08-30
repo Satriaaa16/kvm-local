3e18059439c03be6b65e55b3dfb78a39d486446b
satria16alan <137033041+Satriaaa16@users.noreply.github.com>
2026-08-30 13:28:59 +0700

    Update format-img-ova-toqcow2.sh

--- format-img-ova-toqcow2.sh
+++ format-img-ova-toqcow2.sh
@@ -1,13 +1,7 @@
 tar -xvf image/$image.ova
 
-vmdk-files=$(ls image/*vmdk | head -n 1)
+qemu-img convert -O qcow2 image/$image.vmdk image/$image.qcow2
 
-vmdk_file=$(ls image/*.vmdk | head -n 1)
-
-image-qcow=$(baseline image/$image.ova ova)
-
-qemu-img convert -f vmdk -O qcow2 "image/$image.vmdk" "image/$image-qcow.qcow2"
-
-sudo cp "image/$image.qcow2" /var/lib/libvirt/images/
+cp image/$image.qcow2 /var/lib/libvirt/images/
 
 ## https://www.obriain.com/primers/ova2kvm.php
