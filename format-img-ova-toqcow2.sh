e0316ad9fc6176cbf0d6cd0506503e98a314a800
satria16alan <137033041+Satriaaa16@users.noreply.github.com>
2026-08-30 13:53:37 +0700

    Update format-img-ova-toqcow2.sh

--- format-img-ova-toqcow2.sh
+++ format-img-ova-toqcow2.sh
@@ -2,10 +2,10 @@ tar -xvf image/$image.ova > disk-output.txt
 
 awk 'NR==2' disk-output.txt > vmdk.txt
 
-export vmdk=(cat vmdk.txt)
+export vmdk=$(cat vmdk.txt)
 
 
-qemu-img convert -O qcow2 image/$vmdk.vmdk image/$image.qcow2
+qemu-img convert -O qcow2 image/$vmdk image/$image.qcow2
 
 cp image/$image.qcow2 /var/lib/libvirt/images/
 
