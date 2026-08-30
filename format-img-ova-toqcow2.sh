c53ab86738cd678c97ee95414824bea5c66e4ee6
satria16alan <137033041+Satriaaa16@users.noreply.github.com>
2026-08-30 12:00:38 +0700

    Update format-img-ova-toqcow2.sh

--- format-img-ova-toqcow2.sh
+++ format-img-ova-toqcow2.sh
@@ -1,7 +1,7 @@
-tar -xvf $vm.ova
+tar -xvf image/$image.ova
 
-qemu-img convert -O qcow2 $vm.vmdk $vm.qcow2
+qemu-img convert -O qcow2 image/$image.vmdk image/$image.qcow2
 
-cp $vm.qcow2 /var/lib/libvirt/images/
+cp image/$image.qcow2 /var/lib/libvirt/images/
 
 ## https://www.obriain.com/primers/ova2kvm.php
