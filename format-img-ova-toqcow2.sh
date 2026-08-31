05c27db0ca29efb35b1aa365ae89d916ee544cef
satria16alan <137033041+Satriaaa16@users.noreply.github.com>
2026-08-31 08:52:43 +0700

    Rename variables for clarity in OVA conversion script

--- format-img-ova-toqcow2.sh
+++ format-img-ova-toqcow2.sh
@@ -1,9 +1,10 @@
 tar -xvf image/$image -C image/
 
-export image-out=$(basename "$image" .ova )
+export imageqc=$(basename "$image" .ova )
+echo $imageqc
 
-qemu-img convert -O qcow2 image/*.vmdk image/$image-out.qcow2
+qemu-img convert -O qcow2 image/*.vmdk image/$imageqc.qcow2
 
-cp image/$image-out.qcow2 /var/lib/libvirt/images/
+cp image/$imageqc.qcow2 /var/lib/libvirt/images/
 
 ## https://www.obriain.com/primers/ova2kvm.php
